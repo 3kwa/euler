9face78f5cd2f9caf5680c266bbcf7f441f9f6a1
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-01-12 16:59:09 +1100

    upper bound explanation

--- 030.py
+++ 030.py
@@ -1,3 +1,14 @@
+"""
+if N has n digits N >= 10**n i.e. 1 followed by n-1 0
+
+we are looking for N who are the sum of their digits so N < n*9**5
+
+the smallest n for which 10**n > n*9**5 is 6
+
+hence no integer above 6*9**5 = 354294 can be the sum of the fifth power of
+its digits
+"""
+
 def is_sum_of_nth_power_of_digits(number, n):
     """
     >>> is_sum_of_nth_power_of_digits(1, 4)
@@ -19,4 +30,4 @@ if __name__ == '__main__':
     import doctest
     doctest.testmod()
 
-    print sum(i for i in range(300000) if is_sum_of_nth_power_of_digits(i, 5))
+    print sum(i for i in range(354294) if is_sum_of_nth_power_of_digits(i, 5))
