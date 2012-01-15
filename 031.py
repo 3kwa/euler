320b112e9f0fa7d86b56dbcdaa114067590d7171
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-01-15 19:04:06 +1100

    recursive problem 31

--- 031.py
+++ 031.py
@@ -1,7 +1,12 @@
+"""
+Exploration first ... and elegant recursive solution later ;)
+
 from operator import mul
 
 REF = [1, 2, 5, 10, 20, 50, 100, 200]
-target = 200
+TARGET = target = 200
+
+# The combinatorial explosion ...
 
 for target in (1, 2, 5, 10, 20, 50, 100, 200):
     print reduce(mul, (target / coin + 1 for coin in REF))
@@ -13,6 +18,7 @@ total = 0
 loops = 0
 t1 = 200
 for i in range(t1/1+1):
+    # reducing the combinatorial factor every iteration ...
     t2 = t1 - i * 1
     for j in range(t2/2+1):
         t3 = t2 - j * 2
@@ -32,3 +38,48 @@ for i in range(t1/1+1):
                                 loops += 1
 print total
 print loops
+"""
+
+class Singleton(object):
+    """
+    Using a Singleton beats using a global in the recursion, if for no other
+    reason than : tricky to declare a global in a doctest :D
+    """
+    value = 0
+
+def recursion_singleton(coins, target, count):
+    """
+    >>> count = Singleton()
+    >>> recursion_singleton([1, 2, 5, 10, 20, 50, 100, 200], 20, count)
+    >>> print count.value
+    41
+    """
+    if target == 0:
+        count.value += 1
+        return
+    if len(coins) == 0:
+        return
+    for n in range(target/coins[0] + 1):
+        recursion_singleton(coins[1:], target - n * coins[0], count)
+
+
+def recursion_genexp(coins, target):
+    """
+    Maybe more elegant but two (2) times slower than the recursion with singleton
+
+    >>> recursion_genexp([1, 2, 5, 10, 20, 50, 100, 200], 20)
+    41
+    """
+    if target == 0:
+        return 1
+    if len(coins) == 0:
+        return 0
+    return sum(recursion_genexp(coins[1:], target - n * coins[0]) for n in range(target/coins[0] + 1))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    count = Singleton()
+    recursion_singleton([1, 2, 5, 10, 20, 50, 100, 200], 200, count)
+    print count.value
