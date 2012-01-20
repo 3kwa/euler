6973b0ab6545fa171586488054f14f998b4dae0c
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-01-20 12:03:16 +1100

    refactored 49

--- 049.py
+++ 049.py
@@ -9,30 +9,22 @@ candidates = defaultdict(list)
 
 for prime in four_digits_prime:
 
-    permutations_of_prime = map(
+    # generating a set of all the permutations of the digit of prime
+    permutations_of_prime = set(map(
         lambda tuple_:int(''.join(tuple_)),
-        permutations(str(prime)))
-    permutations_of_prime.remove(prime)
+        permutations(str(prime))))
 
+    # for every permutation
     for permutation in permutations_of_prime:
-        if permutation in four_digits_prime:
+        # check for primality, 3330 and greater than prime (key is smallest prime)
+        if permutation in four_digits_prime and \
+           (permutation - prime) % 3330 == 0 and \
+           permutation > prime:
             candidates[prime].append(permutation)
 
-def sequence_of_three(list_):
-    differences = defaultdict(set)
-
-    for index, value in enumerate(list_):
-        for i in range(index + 1, len(list_)):
-            other = list_[i]
-            differences[abs(value-other)].update(set([value, other]))
-
-    for k,v in differences.iteritems():
-        if len(v) == 3 and k == 3330:
-            return v
-
+# display the concatenated string of 3 primes satisfying the conditions
 for k,v in candidates.iteritems():
     v.append(k)
-    man = sequence_of_three(v)
-    if man is not None:
-        print man
+    if len(v) == 3:
+        print ''.join(map(str,sorted(v)))
 
