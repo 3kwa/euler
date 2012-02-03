ae4b124c8a3e23e958a8a01353c54b3a09955bad
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-02-03 15:37:02 +1100

    073 in 15s ... being smarter ;)

--- 073.py
+++ 073.py
@@ -1,12 +1,39 @@
-from fractions import Fraction
+"""
+brute force 7295372 19m
+smart runs in 15s
+"""
 
-D = 12000
-result = set()
-lower = Fraction(1, 3)
-upper = Fraction(1, 2)
-for d in range(2, D + 1):
-    for n in range(1, d + 1):
-        man = Fraction(n, d)
-        if lower < man < upper:
-            result.add(man)
-print len(result)
+from fractions import Fraction, gcd
+
+def brute(D):
+    """
+    >>> brute(8)
+    3
+    """
+    result = set()
+    lower = Fraction(1, 3)
+    upper = Fraction(1, 2)
+    for d in range(2, D + 1):
+        for n in range(1, d + 1):
+            man = Fraction(n, d)
+            if lower < man < upper:
+                result.add(man)
+    return len(result)
+
+def smart(D):
+    """
+    >>> smart(8)
+    3
+    """
+    count = 0
+    for d in range(3, D+1):
+        start = int( 1. / 3 * d) + 1
+        stop = int(1. / 2 * d) + 1
+        for n in range(start, stop):
+            if gcd(d, n) == 1:
+                count += 1
+    return count
+
+import doctest
+doctest.testmod()
+print smart(12000)
