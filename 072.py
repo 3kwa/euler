4213eab3d7f4270b19ebe037da5cbe84620953c9
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-02-03 13:44:37 +1100

    factors is memoized hence much faster - 86s -> 38s

--- 072.py
+++ 072.py
@@ -5,7 +5,35 @@ from primes import generator
 from millerrabin import is_probable_prime
 
 
-def factors(number, result=[]):
+# http://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+class memoized(object):
+   """Decorator that caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned, and
+   not re-evaluated.
+   """
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      try:
+         return self.cache[args]
+      except KeyError:
+         value = self.func(*args)
+         self.cache[args] = value
+         return value
+      except TypeError:
+         # uncachable -- for instance, passing a list as an argument.
+         # Better to not cache than to blow up entirely.
+         return self.func(*args)
+   def __repr__(self):
+      """Return the function's docstring."""
+      return self.func.__doc__
+   def __get__(self, obj, objtype):
+      """Support instance methods."""
+      return functools.partial(self.__call__, obj)
+
+@memoized
+def factors(number):
     """
     >>> factors(3)
     [3]
@@ -16,15 +44,16 @@ def factors(number, result=[]):
     >>> factors(12)
     [2, 3]
     """
-    if number == 1:
-        return result
     if is_probable_prime(number):
-        return result + [number]
+        return [number]
     for p in generator():
         if number % p == 0:
             while number % p == 0:
                 number = number / p
-            return factors(number, result +[p])
+            if number == 1:
+                return [p]
+            else:
+                return [p] + factors(number)
 
 def totient(n):
     """
